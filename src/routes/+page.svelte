d6bcb2632b17ed0b31331fc86352d7a41e17ab94
Daan Carstens <daanbrother@icloud.com>
2023-09-06 15:35:52 +0200

    data uit prismic

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { SliceZone } from '@prismicio/svelte'
+    import { components } from '$lib/slices' 
+
+    export let data
+</script>
+
+<h1>{ data.title }</h1>
+<p>{ data.content[0].text}</p>
+<img src="{ data.image.url }" alt="">
+
+<SliceZone slices={data.slices} {components} />
